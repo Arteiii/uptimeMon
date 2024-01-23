a51032224c8b2ae47230d99cea2cc5f49f92a5ac
Ben <ben.arteii@proton.me>
2024-01-23 23:42:09 +0100

    changes to fix css not wokring in docker

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,11 +1,15 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
+  important: true,
   darkMode: "class",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // using ./src/ dir
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // using ./ dir
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // add more paths here
   ],
   theme: {
     extend: {
